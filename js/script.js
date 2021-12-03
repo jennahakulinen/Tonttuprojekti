bab2d6a6e43493189b79420336a1017ef0195648
idaols <ida.olsson@metropolia.fi>
2021-12-03 11:25:28 +0200

    uusi navigaatio

--- js/script.js
+++ js/script.js
@@ -1,79 +1,119 @@
 const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".navigationNav");
+const menuBtn = document.querySelector(".hamBut");
+const searchBtn = document.querySelector(".searchBut");
+const userBtn = document.querySelector(".userBut");
+const closeBtn = document.querySelector(".closeBut");
 const userMenu = document.querySelector(".user-menu");
 const user = document.querySelector(".user");
 const search = document.querySelector(".search");
+const body = document.querySelector("body");
 
-/* Toggle mobile menu */ //nuoli funktiot!
-const toggleMenu = () => {
-  menu.classList.toggle('active');
 
-  if (userMenu.classList.contains('active')) {
-    userMenu.classList.remove('active');
-  }
-  if (menu.classList.contains("active")) {
-    // adds the close (x) icon
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-times-circle'></i>";
-  } else {
-    // adds the menu (hamburger) icon
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-  }
+const disableScrolling = () => {
+  let x = window.scrollX;
+  let y = window.scrollY;
+  window.onscroll = () => {
+    window.scrollTo(x, y);
+  };
 }
 
-// Toggle user
-
-const toggleUser = () => {
-  userMenu.classList.toggle('active');
-
-  if (menu.classList.contains('active')) {
-    menu.classList.remove('active');
-  }
-  if (userMenu.classList.contains("active")) {
-    // adds the close (x) icon
-    user.querySelector("a").innerHTML = "<i class='fas fa-times-circle'></i>";
-  } else {
-    // adds the user icon
-    user.querySelector("a").innerHTML = '<i class="fas fa-user"></i>';
-  }
+const enableScrolling = () => {
+  window.onscroll = () => {
+  };
 }
 
-/* Event Listener */
-toggle.addEventListener("click", toggleMenu, false);
-
-const items = document.querySelectorAll(".item");
-
-user.addEventListener("click", toggleUser, true);
-
-
-
-/* Activate Submenu */
-const toggleItem = (evt) => {
-  if (evt.currentTarget.classList.contains("submenu-active")) {
-    evt.currentTarget.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    evt.currentTarget.classList.add("submenu-active");
-  } else {
-    evt.currentTarget.classList.add("submenu-active");
-  }
+menuBtn.onclick = () => {
+  menu.style.visibility = "visible";
+  menu.style.right = "0";
+  closeBtn.style.display = "block";
+  menuBtn.style.display = "none";
+  searchBtn.style.display = "none";
+  userBtn.style.display = "none";
+  disableScrolling();
 }
 
-/* Event Listeners */
-for (let item of items) {
-  if (item.querySelector(".submenu")) {
-    item.addEventListener("click", toggleItem, false);
-    item.addEventListener("keypress", toggleItem, false);
-  }
+closeBtn.onclick = () => {
+  menu.style.visibility = "hidden";
+  menu.style.right = "-100%";
+  menuBtn.style.display = "block";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "block";
+  searchBtn.style.display = "block";
+  userBtn.style.display = "block";
+  enableScrolling();
 }
 
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-  let isClickInside = menu.contains(e.target);
+// /* Toggle mobile menu */ //nuoli funktiot!
+// const toggleMenu = () => {
+//   menu.classList.toggle('active');
 
-  if (!isClickInside && menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-  }
-}
+//   if (userMenu.classList.contains('active')) {
+//     userMenu.classList.remove('active');
+//   }
+//   if (menu.classList.contains("active")) {
+//     // adds the close (x) icon
+//     toggle.querySelector("a").innerHTML = "<i class='fas fa-times-circle'></i>";
+//   } else {
+//     // adds the menu (hamburger) icon
+//     toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+//   }
+// }
 
-/* Event listener */
-document.addEventListener("click", closeSubmenu, false);
+// // Toggle user
+
+// const toggleUser = () => {
+//   userMenu.classList.toggle('active');
+
+//   if (menu.classList.contains('active')) {
+//     menu.classList.remove('active');
+//   }
+//   if (userMenu.classList.contains("active")) {
+//     // adds the close (x) icon
+//     user.querySelector("a").innerHTML = "<i class='fas fa-times-circle'></i>";
+//   } else {
+//     // adds the user icon
+//     user.querySelector("a").innerHTML = '<i class="fas fa-user"></i>';
+//   }
+// }
+
+// /* Event Listener */
+// toggle.addEventListener("click", toggleMenu, false);
+
+// const items = document.querySelectorAll(".item");
+
+// user.addEventListener("click", toggleUser, true);
+
+
+
+// /* Activate Submenu */
+// const toggleItem = (evt) => {
+//   if (evt.currentTarget.classList.contains("submenu-active")) {
+//     evt.currentTarget.classList.remove("submenu-active");
+//   } else if (menu.querySelector(".submenu-active")) {
+//     menu.querySelector(".submenu-active").classList.remove("submenu-active");
+//     evt.currentTarget.classList.add("submenu-active");
+//   } else {
+//     evt.currentTarget.classList.add("submenu-active");
+//   }
+// }
+
+// /* Event Listeners */
+// for (let item of items) {
+//   if (item.querySelector(".submenu")) {
+//     item.addEventListener("click", toggleItem, false);
+//     item.addEventListener("keypress", toggleItem, false);
+//   }
+// }
+
+// /* Close Submenu From Anywhere */
+// function closeSubmenu(e) {
+//   let isClickInside = menu.contains(e.target);
+
+//   if (!isClickInside && menu.querySelector(".submenu-active")) {
+//     menu.querySelector(".submenu-active").classList.remove("submenu-active");
+//   }
+// }
+
+// /* Event listener */
+// document.addEventListener("click", closeSubmenu, false);
