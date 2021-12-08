68724a339728d2d3196fd63834570db9cba37825
jennahakulinen <jenna.hakulinen@metropolia.fi>
2021-12-08 21:10:11 +0200

    Plusnapit toimii jee

--- js/plusbutton.js
+++ js/plusbutton.js
@@ -1,14 +1,41 @@
-'use strict'
+'use strict';
 
-document.getElementById("addBtn").onclick = newRow;
+const formtagInc = document.getElementById("addMore-includes");
+const formtagStep = document.getElementById("addMore-steps");
+const formtagCategory = document.getElementById("addMore-category");
+const addMoreBtnInc = document.getElementById("addBtnInc");
+const addMoreBtnStep = document.getElementById("addBtnSteps");
+const addMoreCategory = document.getElementById("addBtnCategory");
 
-const newRow = () => {
-    `<div class="recipe-includes">
-    <input type="number" id="recipe-input" name="quantity" min="1" placeholder="2">
-    <input type="text" id="recipe-input" name="unit" max="3" placeholder="dl">
-    <input type="text" id="recipe-input" name="ingredient" placeholder="sokeri">
-    </div>`;
+addMoreBtnInc.onclick = () => {
+    const addMoreIncHtml =
+
+        `<div class="recipe-includes">
+        <input type="number" id="recipe-input" name="quantity" min="1" placeholder="2">
+        <input type="text" id="recipe-input" name="unit" max="3" placeholder="dl">
+        <input type="text" id="recipe-input" name="ingredient" placeholder="sokeri">
+        </div>`;
+
+    formtagInc.innerHTML += addMoreIncHtml;
+}
+    
+addMoreBtnStep.onclick = () => {
+    const addMoreStepHtml =
+
+        `<div class="add-recipe-steps">
+        <input type="number" class="step" id="recipe-input" name="step" min="1" placeholder="1.">
+        <textarea name="description" class="resizable" id="recipe-input" placeholder="Laita uuni päälle"></textarea>
+        </div>`;
+
+    formtagStep.innerHTML += addMoreStepHtml;
 }
 
+addMoreCategory.onclick = () => {
+    const addMoreCategoryHtml =
 
-    
+        `<div class="add-recipe-category">
+        <input type="text" id="recipe-input" name="category" placeholder="Leivonta" class="category">
+        </div>`;
+
+    formtagCategory.innerHTML += addMoreCategoryHtml;
+}
