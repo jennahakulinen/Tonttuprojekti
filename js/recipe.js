5fcfb2988e156a44a86864acb9dfd4b290bd0967
idaols <ida.olsson@metropolia.fi>
2021-12-09 11:48:08 +0200

    kategoriat looppi

--- js/recipe.js
+++ js/recipe.js
@@ -3,18 +3,20 @@
 const recipeStuff = [
     {
         filename: 'img/image10.jpg',
-        title: 'Piparitalo',
+        title: 'Joululetut',
         user: 'BestCook',
         profilepic: 'https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo',
         rates: '4/5',
-        categories: 'Vegaaninen joulu',
         ingredients: 'blabla',
         steps: 'blabla',
-        cooktime: '30 min',
+        cooktime: '30',
     },
 ];
 
+const categoryStuff = ["Vegaaninen joulu", "Leivonta", "Jälkiruoka", "Jouluinen", "Herkku"];
+
 const recipebasis = document.querySelector('.recipebasis');
+
 
 const getRecipe = () => {
     recipebasis.innerHTML = ``;
@@ -24,8 +26,8 @@ const getRecipe = () => {
             <img class="recipe-img" src="${recipeStuff[0].filename}" alt="Reseptin kuva">
         </div>
         <div class="recipe-addfav">
-            <i class="fas fa-heart recipe-fav"></i>
             <i class="far fa-heart recipe-emptyfav"></i>
+            <i class="fas fa-heart recipe-fav"></i>
         </div>
         <div class="recipe-header">
             <div class="recipe-name">
@@ -50,51 +52,74 @@ const getRecipe = () => {
             </div>
         </div>
         <div class="recipe-categories">
-            <div class="recipe-cat-item">${recipeStuff[0].categories}</div>
-        </div>
-    </div>
-
-    <hr class="re-hr">
-
-
-    <div class="col-12 recipe-container">
-        <div class="col-12">
-            <h1 class="header recipe-h">Ainesosat</h1>
-            <div class="col-11 recipe-ingredients">
-                <ul class="recipe-grid-container">
-                    <li class="recipe-grid">2</li>
-                    <li class="recipe-grid">dl</li>
-                    <li class="recipe-grid">korppujauhoja</li>
-                    <li class="recipe-grid">1</li>
-                    <li class="recipe-grid">kpl</li>
-                    <li class="recipe-grid">sitruunaa</li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="col-12">
-            <h1 class="header recipe-h">Ohjeet</h1>
-            <div class="col-11 recipe-steps">
-                <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, in. Repellat
-                        doloremque
-                        earum, vitae alias aliquam debitis laboriosam doloribus. Odio, libero? Doloribus
-                        asperiores
-                        sit
-                        eum nemo repellat aspernatur nisi fugiat?</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi obcaecati odit itaque
-                        ipsam
-                        eos nisi quos ut quae ducimus sequi, facilis rerum beatae architecto nam pariatur
-                        numquam
-                        temporibus. Sapiente, sunt.</li>
-                </ul>
-            </div>
-            <div class="recipe-cookingtime">
-                <i class="far fa-clock cooktime"></i>
-                <p>${recipeStuff[0].cooktime}</p>
-            </div>
         </div>
     </div>`;
+
+    const recipeCategories = document.querySelector('.recipe-categories');
+
+    recipeCategories.innerHTML = ``;
+
+    for (let i = 0; i < categoryStuff.length; i++) {
+        recipeCategories.innerHTML +=
+            `<div class="recipe-cat-item">${categoryStuff[i]}</div>`;
+    }
+
+    recipebasis.innerHTML +=
+        `<hr class="re-hr">
+
+        <div class="col-12 recipe-container">
+            <div class="col-12">
+                <h1 class="header recipe-h">Ainesosat</h1>
+                <div class="col-11 recipe-ingredients">
+                    <ul class="recipe-grid-container">
+                        <li class="recipe-grid">2</li>
+                        <li class="recipe-grid">dl</li>
+                        <li class="recipe-grid">korppujauhoja</li>
+                        <li class="recipe-grid">1</li>
+                        <li class="recipe-grid">kpl</li>
+                        <li class="recipe-grid">sitruunaa</li>
+                    </ul>
+                </div>
+            </div>
+    
+            <div class="col-12">
+                <h1 class="header recipe-h">Ohjeet</h1>
+                <div class="col-11 recipe-steps">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, in. Repellat
+                            doloremque
+                            earum, vitae alias aliquam debitis laboriosam doloribus. Odio, libero? Doloribus
+                            asperiores
+                            sit
+                            eum nemo repellat aspernatur nisi fugiat?</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi obcaecati odit itaque
+                            ipsam
+                            eos nisi quos ut quae ducimus sequi, facilis rerum beatae architecto nam pariatur
+                            numquam
+                            temporibus. Sapiente, sunt.</li>
+                    </ul>
+                </div>
+                <div class="recipe-cookingtime">
+                    <i class="far fa-clock cooktime"></i>
+                    <p>${recipeStuff[0].cooktime} min</p>
+                </div>
+            </div>
+        </div>`;
+
+
+
+
+    const likeBtn = document.querySelector(".recipe-addfav");
+    const heart = document.querySelector(".recipe-fav");
+
+    likeBtn.onclick = () => {
+        if (heart.style.visibility === "visible") {
+            heart.style.visibility = "hidden";
+        } else {
+            heart.style.visibility = "visible";
+        }
+    }
+
 }
 
 getRecipe();
